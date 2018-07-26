821bb1004989c594be6ba71352699b34413e1438
George  Pianka <gpianka1@gmail.com>
2018-07-26 06:12:09 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,6 +11,10 @@ class Scraper
     
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
    end
+   
+   def get_courses 
+     self.get_page.css(".post")
+    end
   
   
   def print_courses
